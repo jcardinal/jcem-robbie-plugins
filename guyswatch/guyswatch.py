625184300d0b6337627fff62dddd26d409393464
Justin Cardinal <justin@somuchuptime.com>
2020-12-28 09:27:56 -0600

    Add guyswatch plugin

---
+++ guyswatch/guyswatch.py
@@ -0,0 +1,16 @@
+import re
+from errbot import BotPlugin, re_botcmd
+
+
+class GuysWatch(BotPlugin):
+    """What if we said "guys" less?"""
+
+    @re_botcmd(pattern=r"(^| )guys( |$)", prefixed=False, flags=re.IGNORECASE)
+    def watch_for_guys(self, msg, match):
+        """If someone says "guys", errbot cringes"""
+        return "Is gender important to understanding this?"
+
+    @re_botcmd(pattern=r"(^| )blue jay( |$)", prefixed=False, flags=re.IGNORECASE)
+    def watch_for_bluejay(self, msg, match):
+        """Try adding a second function to this class"""
+        return "Those are the screech-y ones, right?"
