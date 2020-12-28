67a110511166a2bb507e97c914f9303af3a5bfc3
Justin Cardinal <justin@somuchuptime.com>
2020-12-28 09:36:29 -0600

    Add function to test updating via git repo

--- guyswatch/guyswatch.py
+++ guyswatch/guyswatch.py
@@ -14,3 +14,8 @@ class GuysWatch(BotPlugin):
     def watch_for_bluejay(self, msg, match):
         """Try adding a second function to this class"""
         return "Those are the screech-y ones, right?"
+
+    @re_botcmd(pattern=r"(^| )test git( |$)", prefixed=False, flags=re.IGNORECASE)
+    def watch_for_bluejay(self, msg, match):
+        """Test updating a plugin via git repo"""
+        return "If you're reading this, it's too git"
